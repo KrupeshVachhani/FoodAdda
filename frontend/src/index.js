872191d8666d926c6e40bab721b2f464f18b19aa
Krupesh Vachhani <vachhani.krupesh@gmail.com>
2023-12-24 10:13:08 +0530

    changes in frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.scss';
-import CustomScrollbar from "./components/layoutes/CustomScrollbar";
 import { CartProvider } from "./components/Context/CartContext.jsx";
 
 
@@ -10,11 +9,9 @@ import { CartProvider } from "./components/Context/CartContext.jsx";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CustomScrollbar>
       <CartProvider>
         <App />
       </CartProvider>
-    </CustomScrollbar>
   </React.StrictMode>
 );
 
